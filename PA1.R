9ec5b4a79e97c29b0ddc52989dcb04d385c6b600
Subwarp <subwarp@users.noreply.github.com>
2015-09-19 00:04:20 -0700

    Work in progres on PA1

--- PA1.R
+++ PA1.R
@@ -7,10 +7,27 @@ if (!file.exists("activity.csv")) {
 d <- read.csv("activity.csv")
 
 class(d)
+
+
+
+## Alternative
+#hist(sapply(split(d$steps, d$date), sum),
+#     xlab = "Number of steps in a day", ylab = "Frequency (Number of days)",
+#     main = "Total number steps taken per day")
+
 stepsPerDay = aggregate(x=d[c("steps")], FUN=sum, by = list(day = d$date))
-hist(stepsPerDay$steps, main="Steps Taken Per Day", xlab="Steps Taken", breaks=c(5))
+class(stepsPerDay$day)
+
+library(ggplot2)
+
+hist(stepsPerDay$steps, freq=T,
+     main="Steps Taken Per Day",
+     xlab="Steps Taken")
 
 mean(stepsPerDay$steps, na.rm=T)
 median(stepsPerDay$steps, na.rm=T)
 
 plot()
+
+
+
