f50200cf43ad7df8c3993365f012487a63819e42
Carlos <carlos@tikaldigital.com>
2015-09-19 01:53:30 -0700

    Work in progres on PA1

--- PA1.R
+++ PA1.R
@@ -15,11 +15,9 @@ class(d)
 #     xlab = "Number of steps in a day", ylab = "Frequency (Number of days)",
 #     main = "Total number steps taken per day")
 
-stepsPerDay = aggregate(x=d[c("steps")], FUN=sum, by = list(day = d$date))
+stepsPerDay <- aggregate(x=d[c("steps")], FUN=sum, by = list(day = d$date))
 class(stepsPerDay$day)
-
 library(ggplot2)
-
 hist(stepsPerDay$steps, freq=T,
      main="Steps Taken Per Day",
      xlab="Steps Taken")
@@ -27,7 +25,9 @@ hist(stepsPerDay$steps, freq=T,
 mean(stepsPerDay$steps, na.rm=T)
 median(stepsPerDay$steps, na.rm=T)
 
-plot()
+plot(d$interval, d$steps, type="l")
 
-
+stepsPerInterval <- aggregate(x=d[c("steps")],
+                              FUN=function(e) { mean(e, na.rm=T)},
+                              by = list(interval = d$interval))
 
