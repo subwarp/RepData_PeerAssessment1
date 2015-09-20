649208d795dd290194483937d104455f7e7bacb0
Subwarp <subwarp@users.noreply.github.com>
2015-09-20 04:07:31 -0700

    Work in progress

--- PA1.R
+++ PA1.R
@@ -32,7 +32,7 @@ median(stepsPerDay$steps, na.rm=T)
 
 
 ## 3 What is the average daily activity pattern?
-# 3.1 Make a time series plot (i.e. type = "l") of the 5-minute interval 
+# 3.1 Make a time series plot (i.e. type = "l") of the 5-minute interval
 # (x-axis) and the average number of steps taken, averaged across all days (y-axis)
 avgStepsPerInterval <- aggregate(x=d[c("steps")],
                               FUN=function(e) { mean(e, na.rm=T)},
@@ -44,22 +44,72 @@ plot(avgStepsPerInterval, type="l",
 str(avgStepsPerInterval)
 summarise(avgStepsPerInterval)
 
-# 3.2 Which 5-minute interval, on average across all the days in the dataset, 
+# 3.2 Which 5-minute interval, on average across all the days in the dataset,
 # contains the maximum number of steps?
-avgStepsPerInterval[which.max(avgStepsPerInterval$steps), ]$interval
+avgStepsPerInterval[which.max(avgStepsPerInterval$mean.steps), ]$interval
 
 ### 4 Imputing missing values
-# 4.1 Calculate and report the total number of missing values in the dataset 
+# 4.1 Calculate and report the total number of missing values in the dataset
 # (i.e. the total number of rows with NAs)
 d.na  <- filter(d, is.na(steps))
 # nas  <- d[is.na(d$steps) == TRUE, ]
 
-# 4.2 Devise a strategy for filling in all of the missing values in the dataset. 
-# The strategy does not need to be sophisticated. For example, you could use the 
+# 4.2 Devise a strategy for filling in all of the missing values in the dataset.
+# The strategy does not need to be sophisticated. For example, you could use the
 # mean/median for that day, or the mean for that 5-minute interval, etc.
-library("zoo")
 
-lapply(head(d, 2), function(irec) {
-  print("In Loop")
-  print(irec$steps)
-})
+# Could us but assignment seems to not want it. library("zoo")
+
+
+# create test set
+t <- head(d, 10)
+t <- rbind(t, tail(d[which(d$steps > 0), ]))
+# t[is.na(t$steps), ] // Selects only rows that have NA for steps
+
+
+t[is.na(t$steps)] <- avgStepsPerInterval$interval == t$interval
+avgStepsPerInterval$interval == t[t$interval == 0]
+
+ifelse(is.na(t$steps), avgStepsPerInterval[t$interval], t$steps)
+
+d2 <- d
+d2$steps <- ifelse(is.na(d$steps),
+            avgStepsPerInterval[avgStepsPerInterval$interval == d$interval, ]$mean.steps,
+            d$steps)
+
+d2StepsPerDay <- aggregate(x=d2[c("steps")], FUN=sum, by = list(day = d2$date))
+
+hist(d2StepsPerDay$steps, freq=T,
+     main="Steps Taken Per Day",
+     xlab="Steps Taken")
+mean(d2StepsPerDay$steps, na.rm=T)
+median(d2StepsPerDay$steps, na.rm=T)
+
+### 5 Are there differences in activity patterns between weekdays and weekends?
+### For this part the weekdays() function may be of some help here.
+### Use the dataset with the filled-in missing values for this part.
+
+# 5.1 Create a new factor variable in the dataset with two levels –
+# “weekday” and “weekend” indicating whether a given date is a weekday or weekend day.
+
+# For testing
+#t <- head(d2)
+#t <- rbind(t, tail(d[which(weekdays.Date(as.Date(d$date)) == "Saturday"), ]))
+#t
+
+
+d2$sow <- ifelse(weekdays.Date(as.Date(t$date)) %in% c("Saturday", "Sunday"),
+                 "Weekend", "Weekday")
+
+# 5.2 Make a panel plot containing a time series plot (i.e. type = "l") of the
+# 5-minute interval (x-axis) and the average number of steps taken, averaged
+# across all weekday days or weekend days (y-axis). See the README file in the
+# GitHub repository to see an example of what this plot should look like using
+# simulated data.
+
+d2AvgStepsPerInterval <- aggregate(x=d2[c("steps")], mean,
+                                 by = list(interval = d2$interval))
+
+d2AvgStepsPerInterval  <- rename(d2AvgStepsPerInterval, mean.steps = steps)
+
+d2AvgStepsPerInterval[which.max(d2AvgStepsPerInterval$steps), ]$interval
